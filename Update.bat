8a09906903b963a589d2b25dbc6b5190368d0b61
2Epik4u <justinabossmlg@gmail.com>
2021-01-28 22:23:23 -0500

    oops

---
+++ Update.bat
@@ -0,0 +1,6 @@
+@echo off
+title Toontown Mystery Updator
+git fetch 
+echo Fetched
+git pull --force
+pause
--- Update_TT_Mystery.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git fetch 
-echo Fetched
-git pull --force
-echo Pulled from git
-pause
