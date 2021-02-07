2fa60f9a191a14457ad222fd3a832ea0b9424413
BardKitten <35885621+BardKitten@users.noreply.github.com>
2021-02-06 22:54:35 -0500

    Changes?

--- Update_TT_Mystery.bat
+++ Update_TT_Mystery.bat
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 @echo off
 git fetch 
 echo Fetched
 git pull --force
 pause
+=======
+@echo off
+git fetch 
+echo Fetched
+git pull --force
+pause
+>>>>>>> Stashed changes
