d54f3df9b5db21aa8c99a4ac127862a83f283bf9
2Epik4u <justinabossmlg@gmail.com>
2021-03-18 16:12:03 -0400

    a

--- Start.bat
+++ Start.bat
@@ -2,15 +2,15 @@
 title Toontown Mystery Launcher
 :launcher
 cls
-echo Toontown Mystery Launcher
+echo What do you want to do?
 echo ===============================
 echo Enter 1 to start a server and play the game.
-echo Enter 2 to join a server.
+echo Enter 2 to connect to a server
 echo ===============================
 
 
 choice /C:123 /n /m "Choose: "%1
-if errorlevel ==2 goto joinserver
+if errorlevel ==2 goto testing
 if errorlevel ==1 goto startserver
 
 
@@ -25,40 +25,44 @@ start start-uberdog-server.bat
 start start-astron-cluster.bat
 goto joinserver
 
+:testing
+cls
+cd tools
+goto joinserver
+
+
 
 :joinserver
-cls
-echo Press 1 if you want to play, or press 3 if you want to play Miniserver.
-echo.
+echo Press 1 if you want to play, or press 2 if you want to connect to a server. (You need the ip)
+echo ===============================
 echo #1 - Localhost
-echo #2 - Custom
-echo #3 - Offical server
-echo.
+echo #2 - Connect to a server
+echo ===============================
 
-:selection
 
 set INPUT=-1
 set /P INPUT=Selection: 
-
 if %INPUT%==1 (
     set TTS_GAMESERVER=127.0.0.1
-) else if %INPUT%==3 (
-    SET TTS_GAMESERVER=52.146.35.215
+
 ) else if %INPUT%==2 (
-    echo.
+echo ===============================
+echo Enter the server IP
+echo ===============================
     set /P TTS_GAMESERVER=Gameserver: 
 ) else (
 	goto selection
 )
+cls
 
-echo.
 
 if %INPUT%==2 (
+echo ===============================
+echo Enter a username.
+echo ===============================
     set /P ttsUsername="Username: "
-    set /P ttsPassword="Password: "
-) else if %INPUT%==4 (
-    set /P ttsUsername="Username: "
-    set /P ttsPassword="Password: "
+    cls
+
 ) else (
     set /P TTS_PLAYCOOKIE=Username: 
 )
@@ -82,15 +86,12 @@ echo ===============================
 
 cd ..
 :main
-if %INPUT%==2 (
-    "panda3d/python/python.exe" -m toontown.toonbase.ToontownStartRemoteDB
-) else if %INPUT%==4 (
-    "panda3d/python/python.exe" -m toontown.toonbase.ToontownStartRemoteDB
+if %INPUT%==2 (   
+    "panda3d/python/python.exe" -m toontown.toonbase.ToontownStart
 ) else (
     "panda3d/python/python.exe" -m toontown.toonbase.ToontownStart
 )
 pause
 
 goto launcher
-
 
